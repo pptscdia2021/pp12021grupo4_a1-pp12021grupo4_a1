c2941204afd0a79694f6e7bb0583ce8acbf7da1b
Vir <Vir>
2021-11-12 17:43:50 -0300

    Incorporo la carpeta POO

---
+++ POO/main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 17:00:41 2021
+
+@author: Vir
+"""
+
+from bolsa_madrid import * 
+
+import pandas as pd
+
+if __name__ == "__main__":
+  url = "https://www.bolsamadrid.es/esp/aspx/Mercados/Precios.aspx?indice=ESI100000000"
+  ruta_descarga = ''
+  
+  # Instancio la clase BOLSA_MAD
+  bm = Bolsa_madrid(url,ruta_descarga)
+  result = bm.buscar_datos() # devuelve un aarray
+  print(result)
+  bm.crear_csv(result) # Crea el CSV
+  df = bm.lee_csv() # lee el csv. Crea y Devuelve un DF
+  print(df)
+  print(bm.max_valor())
+  print(bm.min_valor())
+  print(bm.to_max_valor())
+  print(bm.to_min_valor())
+  
+  
+  
